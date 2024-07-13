6571e4b01e834cc4ee17aacd52362a9f7903a40e
Erik Kietzman <ErikKietzman@gmail.com>
2024-07-13 14:47:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ def main():
     print("Welcome to the tip calculator \n")
     bill = float(input("What was the total bill? \n$"))
     tip_percent = int(input("What tip percentage would you like to give? (ex. 10, 12, 15...) \n")) / 100
-    tip = bill * tip_percent
-    final_bill = bill + tip
+    final_bill = bill + (bill*tip_percent)
     people = int(input("How many people to split the bill? \n"))
     answer = (final_bill / people)
 
-    print(f"The formatted price is ${answer:,.2f}")
+    print(f"Each person should pay: ${answer:,.2f}")
 
 
 if __name__ == '__main__':
